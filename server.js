22ce8633384c1eb35b0bb4f7dc12f5450d2449ae
Luciano Paci <lucianopaci93@gmail.com>
2022-05-08 13:21:28 -0300

    Fixed React App mounting

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const Koa = require('koa')
-
+const Router = require('koa-router')
 const cors = require('@koa/cors')
 const bodyParser = require('koa-body')
 const mount = require('koa-mount')
+const serve = require('koa-static')
+const path = require('path')
+const fs = require('fs')
 
 /* API Routers */
 const apiRouters = require('./src/routers')
 
-// You will have 3 Koa Apps (client, server, client + server)
+// We will have 3 Koa Apps (client, server, client + server)
 /**
  * Koa app (client + server)
  */
 const app = new Koa()
+
+/**
+ * Koa client (React App)
+ */
+const client = new Koa()
+const router = new Router()
+
+function index(ctx) {
+  ctx.body = fs.readFileSync(
+    path.resolve(path.join('client/build', 'index.html')), // We specify where will be the client app
+    'utf8'
+  )
+}
+
+router.get('/', index)
+// router.get('/*', index)
+client.use(serve(path.resolve('client/build'))) // We specify where you want to serve your client app
+client.use(router.routes())
 
 /**
  * Koa server
@@ -27,11 +48,12 @@ server.use(cors())
 server.use(apiRouters.routes())
 server.use(apiRouters.allowedMethods())
 
-app.use(mount('/api', server)) // You will access the server app from here
+app.use(mount('/api', server)) // We will access the server app from here
+app.use(mount('/client', client)) // We will access the client app from here
 
 const config = {
   appName: 'Google Maps App',
-  port: 4000,
+  port: 4000, // Backend Port
   environment: 'development',
 }
 
