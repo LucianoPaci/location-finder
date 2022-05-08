1ce0cd67a2f75ea1c92727ea1813fd9b74d415c6
Luciano Paci <lucianopaci93@gmail.com>
2022-05-08 15:00:13 -0300

    Added Basic layout for Results Page

--- client/src/pages/ResultPage/index.jsx
+++ client/src/pages/ResultPage/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
 function ResultPage() {
+  const navigate = useNavigate()
+
   return (
-    <Box component={'main'} sx={{ border: '1px solid black' }}>
-      ResultPage
+    <Box component={'main'}>
+      <Box sx={{ justifyContent: 'end', display: 'flex' }}>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: 'black', borderRadius: 8, m: 2 }}
+          onClick={() => navigate('/')}
+        >
+          Exit
+        </Button>
+      </Box>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        <h1>Address</h1>
+      </Box>
+      <Box
+        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      >
+        MAP
+      </Box>
     </Box>
   )
 }
